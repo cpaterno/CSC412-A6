e6569f586427677fa38e1a7ff994981ffe1daf64
cpaterno <cpaterno@my.uri.edu>
2019-04-28 15:30:57 -0400

    Mostly there need to do some fine tuning

--- Prog06_ChristopherPaterno/Version1/imageThread.cpp
+++ Prog06_ChristopherPaterno/Version1/imageThread.cpp
@@ -9,8 +9,46 @@ enum Colors {
     ALPHA	
 };
 
-long slidingWindow(long targetR, long targetC, const unsigned char* currentImage, ) {
+unsigned char uCharMax(unsigned char a, unsigned char b) {
+    return ((a > b) ? a : b);
+}
 
+long slidingWindow(long targetR, long targetC, const unsigned char* currentImage, unsigned int perRow, unsigned int perPixel, long imgHeight, long imgWidth) {
+    unsigned char min, max, gray;
+    min = 255;
+    max = gray = 0;
+    long startR, startC, endR, endC, index;
+    startR = targetR - (winSize / 2);
+    startC = targetC - (winSize / 2);
+    endR = targetR + (winSize / 2);
+    endC = targetC + (winSize / 2);
+    // handle case of even winSize by left bias (assuming target pixel is on the left half of window)
+    if (winSize % 2 == 0) {
+        ++startR;
+        ++startC;
+    }
+    // bound checking
+    if (startR < 0)
+        startR = 0;
+    if (startC < 0)
+        startC = 0;
+    // result will be negative if overflow occurs
+    if (endR < 0 || endR >= imgHeight)
+        endR = imgHeight - 1;
+    if (endC < 0 || endC >= imgWidth)
+        endC = imgHeight - 1;
+
+    for (long i = startR; i <= endR; ++i) {
+        for (long j = startC; j <= endC; ++j) {
+            index = i * perRow + j * perPixel;
+            gray = uCharMax(uCharMax(currentImage[index + RED], currentImage[index + GREEN]), currentImage[index + BLUE]);
+            if (gray < min)
+                min = gray;
+            if (gray > max)
+                max = gray;
+        }
+    }
+    return max - min;
 }
 
 void* imageThreadFunc(void* arg) {
@@ -31,7 +69,7 @@ void* imageThreadFunc(void* arg) {
             // image currently looking at in image stack
             for (std::size_t k = 0; k < imageInfo->imageStack->size(); ++k) {
                 inPixels = static_cast<const unsigned char*>(imageInfo->imageStack->at(k)->raster);
-                tempDiff = slidingWindow(i, j, inPixels, imageInfo->imageStack->at(k)->);
+                tempDiff = slidingWindow(i, j, inPixels, imageInfo->imageStack->at(k)->bytesPerRow, imageInfo->imageStack->at(k)->bytesPerPixel, imageInfo->imageStack->at(k)->height, imageInfo->imageStack->at(k)->width);
                 if (tempDiff > maxDiff) {
                     maxDiff = tempDiff;
                     diffIndex = k;
@@ -42,7 +80,7 @@ void* imageThreadFunc(void* arg) {
             outPixels[pixelIndex + RED] = inPixels[pixelIndex + RED];
             outPixels[pixelIndex + GREEN] = inPixels[pixelIndex + GREEN];
             outPixels[pixelIndex + BLUE] = inPixels[pixelIndex + BLUE];
-            outPixels[pixelIndex + ALPHA] = inPixels[pixelIndex + ALPHA];
+            //outPixels[pixelIndex + ALPHA] = inPixels[pixelIndex + ALPHA];
 
         }
     }
